b41dd93d40b4c6812ab327f6d948cd9a73f2781a
Gabriel Godoy <gabyreload@gmail.com>
2023-11-09 11:05:55 -0300

    server action to submit new ticket

---
+++ app/(dashboard)/tickets/actions.js
@@ -0,0 +1,23 @@
+"use server";
+
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function addTicket(formData) {
+  const ticket = Object.fromEntries(formData);
+
+  const supebase = createServerActionClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supebase.auth.getSession();
+
+  const { error } = await supebase
+    .from("tickets")
+    .insert({ ...ticket, user_email: session.user.email });
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
+}
